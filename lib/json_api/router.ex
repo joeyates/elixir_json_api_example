3402173c25d071fcbc1b45d5c368e47c05d0a606
Joe Yates <joe.g.yates@gmail.com>
2015-01-10 14:58:15 +0100

    Handle 404s

--- lib/json_api/router.ex
+++ lib/json_api/router.ex
@@ -1,6 +1,16 @@
 defmodule JsonApi.Router do
   use Plug.Router
 
+  defmodule NotFound do
+    defexception [message: "Not Found"]
+
+    defimpl Plug.Exception do
+      def status(_exception) do
+        404
+      end
+    end
+  end
+
   plug :match
   plug :dispatch
 
@@ -9,5 +19,9 @@ defmodule JsonApi.Router do
     body = Poison.encode!(body, pretty: true)
     put_resp_header(conn, "content-type", "application/json")
     send_resp(conn, 200, body)
+  end
+
+  match _ do
+    raise NotFound
   end
 end
